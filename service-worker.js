79ac68381385a2f463021b1a39a129383786f68f
Raymessco <raymessco@gmail.com>
2025-09-07 22:40:27 -0500

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,20 +2,30 @@ const CACHE_NAME = 'raymessco-v1';
 const urlsToCache = [
   '/',
   '/index.html',
+  'https://cdn.tailwindcss.com',
   '/icon-192.png',
   '/icon-512.png'
+  // No incluyas los scripts de Firebase aquí, ya que necesitan conexión.
 ];
 
+// Instalar el Service Worker y guardar los archivos en caché
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
+// Interceptar las peticiones y servir desde el caché si es posible
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
+      .then(response => {
+        // Si el recurso está en caché, lo devuelve. Si no, lo busca en la red.
+        return response || fetch(event.request);
+      })
   );
 });
