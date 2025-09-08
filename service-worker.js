a4c2d954b06b9f95e588a075aa65ec6786101d8e
Raymessco <raymessco@gmail.com>
2025-09-07 22:56:46 -0500

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,30 +1,33 @@
-const CACHE_NAME = 'raymessco-v1';
+const CACHE_NAME = 'raymessco-cache-v1';
 const urlsToCache = [
   '/',
   '/index.html',
   'https://cdn.tailwindcss.com',
   '/icon-192.png',
-  '/icon-512.png'
-  // No incluyas los scripts de Firebase aquí, ya que necesitan conexión.
+  '/icon-512.png',
+  'https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js',
+  'https://www.gstatic.com/firebasejs/10.12.2/firebase-auth-compat.js',
+  'https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore-compat.js'
 ];
 
-// Instalar el Service Worker y guardar los archivos en caché
+// Instala el Service Worker y guarda los archivos estáticos en el caché.
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => {
-        console.log('Opened cache');
+        console.log('Cache abierto');
         return cache.addAll(urlsToCache);
       })
   );
 });
 
-// Interceptar las peticiones y servir desde el caché si es posible
+// Intercepta las peticiones de red.
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
-        // Si el recurso está en caché, lo devuelve. Si no, lo busca en la red.
+        // Si el recurso está en el caché, lo devuelve.
+        // Si no, intenta obtenerlo de la red.
         return response || fetch(event.request);
       })
   );
